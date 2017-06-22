df8ae6586ae7ad2852f18a919302347507f23254
sakazume <t-sakaume@u-note.co.jp>
2017-06-22 19:31:02 +0900

    偽物語対応

--- src/main/Betch/nise/Main.java
+++ src/main/Betch/nise/Main.java
@@ -1,5 +1,6 @@
 package nise;
 
+import lombok.Data;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -7,8 +8,7 @@ import pachinko.library.JsoupHelper2;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by gyutr20 on 2017/06/05.
@@ -24,20 +24,30 @@ public class Main {
         Elements els = getListLink(doc);
         List<Document> docList = new ArrayList<>();
 
+        Map<String,Elements> map = new HashMap<>();
+
         els.forEach(s->{
             String href = s.attr("href");
             String text = s.text();
             String niseUrl = domain + href;
             Document dataDoc = JsoupHelper2.run(niseUrl);
-            docList.add(dataDoc);
-        });
 
+            Elements slotDataEls = getSlotDataEls(dataDoc);
+            map.put(text,slotDataEls);
+        });
         JsoupHelper2.end();
-        getDate(-1);
+
+        for(Map.Entry<String,Elements> ent : map.entrySet()){
+            for (Element element : ent.getValue()) {
+              new UnitData(element);
+            }
+        }
+
+
     }
 
     /**
-     * お店の機種一覧から台リンクのエレメントを取得する
+     * お店の機種個別一覧から台リンクのエレメントを取得する
      * @param doc
      * @return
      */
@@ -53,9 +63,33 @@ public class Main {
         //        d.plusDays(i);
     }
 
-    public static Elements getSlotData(Document doc) {
+    public static Elements getSlotDataEls(Document doc) {
         return doc.select(".data tbody tr");
     }
 
+    @Data
+    public static class UnitData {
+        String BB回数;
+        String RB回数;
+        String BB確率;
+        String 合成回数;
+        String 総スタート;
+        String 最終スタート;
+        String 最大メダル;
+        String 日付;
+        Date date;
 
+        public UnitData(Element el) {
+            Elements els = el.select("td");
+            BB回数 = els.get(1).text();
+            RB回数 = els.get(2).text();
+            BB確率 = els.get(3).text();
+            合成回数 = els.get(4).text();
+            総スタート = els.get(5).text();
+            最終スタート = els.get(6).text();
+            最大メダル = els.get(7).text();
+            日付 = els.get(0).text();
+
+        }
+    }
 }
